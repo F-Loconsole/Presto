72cea2d2c44eeb4c3b371ded8248febd67b690df
F-Loconsole <france.loconsole@gmail.com>
2023-03-07 10:38:51 +0100

    Completo di card con slide show

--- main.js
+++ main.js
@@ -31,3 +31,59 @@ window.addEventListener('scroll', ()=>{
         logoBlue.classList.add("d-none");
    }
 });
+// test attributo custom
+let test = document.querySelector("#test");
+// metodo per assegnare un attirbuto nuov0 ad un tag = .setAttribute()
+// accetta 2 parametri stringa obbigatori:
+// - nome attributo
+// - valore
+
+// Esempio di accesso ad un attributo aggiunto
+// test.setAttribute("data-newAttrib", "Un saluto a tutti");
+// test.addEventListener('click', ()=>{
+//     console.log(test.dataset.newattrib);
+// });
+
+// Esempio di accesso ad un attributo di un altro tag
+// test.setAttribute("data-newAttrib", '#test-img');
+// test.addEventListener('click', ()=>{
+//     let test_img = document.querySelector(test.dataset.newattrib);
+//     test_img.classList.add('bg-warning');
+// });
+
+// --------------- setInterval -----------
+let firstSpan = document.querySelector("#first-Span");
+let secondSpan = document.querySelector("#second-Span");
+let thirdSpan = document.querySelector("#third-Span");
+function createInterval(finalNumber, element){
+    let counter = 0;
+    let interval = setInterval( ()=> {
+        // console.log(counter);
+        if (counter < finalNumber){
+            counter++;
+            element.innerHTML = counter;
+        }
+        else
+            clearInterval(interval);
+    }, 1)
+}
+let primaVolta = true;
+// ------------- intersectionObserver() ----------
+// oggetto precostruito del browser
+// cattura gli elementi da visualizzare e lancia una funzione anonima
+// riceve come parametro: ENTRIES elemento da osservare
+// appena si osserva l'oggetto lancia la funzione
+// per ogni entries che incontra ...
+let h2Test = document.querySelector("#h2Test");
+let observer = new IntersectionObserver( (entries)=>{
+    entries.forEach( (entry) => {
+        if (entry.isIntersecting && primaVolta) {
+            // console.log('msg');
+            createInterval(1000, firstSpan);
+            createInterval(2000, secondSpan);
+            createInterval(3000, thirdSpan);
+            primaVolta = false;
+        }
+    });
+});
+observer.observe(h2Test);
